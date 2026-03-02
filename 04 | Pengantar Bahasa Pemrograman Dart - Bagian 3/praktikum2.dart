329982c639be098042c82eda4278b96dd6e8a663
aadinda75 <dindaluluk209@gmail.com>
2026-03-02 23:50:40 +0700

    Add files via upload

---
+++ 04 | Pengantar Bahasa Pemrograman Dart - Bagian 3/praktikum2.dart
@@ -0,0 +1,14 @@
+void main() {
+  var names1 = <String>{};
+  Set<String> names2 = {};
+  var names3 = {}; 
+
+  names1.add("Adinda Luluk Hanifah");
+  names1.add("244107060127");
+
+  names2.addAll(["Adinda Luluk Hanifah", "244107060137"]);
+
+  print(names1);
+  print(names2);
+  print(names3);
+}
